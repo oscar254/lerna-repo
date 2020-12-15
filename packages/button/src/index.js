68dbea539bf7730f9368b145f1d76323ed666bd1
Eduardo Rengifo <eduardorengifo5@gmail.com>
2020-12-15 08:51:25 -0500

    Update border radius final

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Button = styled.button`
     background: red;
     color: #fff;
-    border-radius: 4px;
+    border-radius: 5px;
     cursor: pointer;
     font-size: 1rem;
     font-weight: 300;
--- packages/input/src/index.js
+++ packages/input/src/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Input = styled.input`
     border: 1px solid #ccc;
-    border-radius: 5px;
+    border-radius: 4px;
     box-sizing: border-box;
     font-size: 16px;
     font-weight: 300;
